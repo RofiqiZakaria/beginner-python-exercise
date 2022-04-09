4b7529469c1b33996c8b1c4b0122babc2840ea07
Rofiqi Zakaria <rofiqizakaria@gmail.com>
2022-04-09 20:51:37 +0700

    menambahkan function intoWeton

--- weton_pro.py
+++ weton_pro.py
@@ -1,5 +1,7 @@
-def intoTheWeton():
+def intoWeton():
     print ("helloo world")
+    print ("this is RZ")
+    print ("after this, we will mf next time")
     
 if __name__ == '__main__':
-    intoTheWeton()
+    intoWeton()

36df1528e777b913acea9f77a7938ba538c64b40
Rofiqi Zakaria <rofiqizakaria@gmail.com>
2022-04-11 09:48:26 +0700

    coba commit via remote

--- weton_pro.py
+++ weton_pro.py
@@ -2,6 +2,8 @@ def intoWeton():
     print ("helloo world")
     print ("this is RZ")
     print ("after this, we will mf next time")
+    print ("telah terduplicate / terclone di local")
     
 if __name__ == '__main__':
     intoWeton()
+    
